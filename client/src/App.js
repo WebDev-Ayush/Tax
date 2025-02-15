3d30d7e9137fb3478865d072a834460be1437d52
Dhruv Rastogi <dhruv.rastogi.2004@gmail.com>
2025-02-16 01:03:09 +0530

    Added client folder correctly

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/')
+      .then(response => setMessage(response.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Full Stack App</h1>
+      <p>Backend says: {message}</p>
+    </div>
+  );
+}
+
+export default App;
+
